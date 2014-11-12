97138715810ddbc9bcab0a85fd73b612f4054e01
Matthew Kuhlke <matthewkuhlke@gmail.com>
2014-11-12 01:27:35 -0800

    tweaked

--- Snoozecontrol/Snoozecontrol/AppDelegate.m
+++ Snoozecontrol/Snoozecontrol/AppDelegate.m
@@ -66,8 +66,6 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits
-    [[AlarmManager sharedManager] disableAlarm];
-    [self stopBase];
     NSLog(@"resigning Entering background!");
 
 }
@@ -78,8 +76,6 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-
-    [self stopBase];    
 }
 
 - (void)startBase {
